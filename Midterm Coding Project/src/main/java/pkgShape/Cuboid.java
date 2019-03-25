cc93a38bbba7bc0f898621d64541db948b373b14
Robert Stahl <rstahl@udel.edu>
2019-03-25 18:17:04 -0400

    Finishes Cuboid Methods

--- Midterm Coding Project/src/main/java/pkgShape/Cuboid.java
+++ Midterm Coding Project/src/main/java/pkgShape/Cuboid.java
@@ -6,5 +6,25 @@ public class Cuboid extends Rectangle {
 	super(width, length);
 	iDepth=depth;
 }
+	public int getDepth() {
+		return iDepth;
+	}
+	public void setDepth(int depth) throws IllegalArgumentException{
+		if(depth<=0) {
+			throw new IllegalArgumentException("Depth must be positive.");
+		}
+		iDepth=depth;
+	}
+	public double volume() {
+		return (double) super.getLength()*super.getWidth()*iDepth;
+	}
+	@Override
+	public double perimeter() throws UnsupportedOperationException{
+		throw new UnsupportedOperationException("Perimeter cannot be calculated for a Cuboid.");
+	}
+	@Override
+	public double area() {
+		return (double)(2*super.getWidth()*super.getWidth()+2*super.getWidth()*iDepth+2*super.getLength()*iDepth);
+	}
 	
 }

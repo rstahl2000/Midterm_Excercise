517c5c01386259ad75fd325560f292b7bb91c2e5
Robert Stahl <rstahl@udel.edu>
2019-03-25 13:51:50 -0400

    Add Rectangle

---
+++ Midterm Coding Project/src/main/java/pkgShape/Rectangle.java
@@ -0,0 +1,22 @@
+package pkgShape;
+
+public class Rectangle extends Shape {
+	private int iWidth;
+	private int iLength;
+	public Rectangle(int width, int length) {
+		iWidth=width;
+		iLength=length;
+	}
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double perimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

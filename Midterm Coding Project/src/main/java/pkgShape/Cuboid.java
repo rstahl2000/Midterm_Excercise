eecbcca29979c7ebac7ad56948139662a03f761e
Robert Stahl <rstahl@udel.edu>
2019-03-25 15:41:16 -0400

    Implements Cuboid

---
+++ Midterm Coding Project/src/main/java/pkgShape/Cuboid.java
@@ -0,0 +1,10 @@
+package pkgShape;
+
+public class Cuboid extends Rectangle {
+	private int iDepth;
+	public Cuboid(int width, int length, int depth) {
+	super(width, length);
+	iDepth=depth;
+}
+	
+}

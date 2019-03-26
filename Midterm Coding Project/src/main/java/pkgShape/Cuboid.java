d3d5bce8e2a102a4df420ab702a15a0371549c36
Robert Stahl <rstahl@udel.edu>
2019-03-25 21:51:31 -0400

    Fixed error in Cuboid and added Cuboid test cases

--- Midterm Coding Project/src/main/java/pkgShape/Cuboid.java
+++ Midterm Coding Project/src/main/java/pkgShape/Cuboid.java
@@ -3,9 +3,12 @@ package pkgShape;
 
 public class Cuboid extends Rectangle {
 	private int iDepth;
-	public Cuboid(int width, int length, int depth) {
+	public Cuboid(int width, int length, int depth)throws IllegalArgumentException {
 	super(width, length);
 	iDepth=depth;
+	if (iDepth<=0) {
+		throw new IllegalArgumentException("Depth must be positive.");
+	}
 }
 	public int getDepth() {
 		return iDepth;
@@ -25,7 +28,7 @@ public class Cuboid extends Rectangle {
 	}
 	@Override
 	public double area() {
-		return (double)(2*super.getWidth()*super.getWidth()+2*super.getWidth()*iDepth+2*super.getLength()*iDepth);
+		return (double)(2*super.getWidth()*super.getLength()+2*super.getWidth()*iDepth+2*super.getLength()*iDepth);
 	}
 	
 	
---
+++ Midterm Coding Project/src/test/java/pkgTestShape/CuboidTest.java
@@ -0,0 +1,83 @@
+package pkgTestShape;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pkgShape.Cuboid;
+
+public class CuboidTest {
+
+	@Test
+	public void getDepthTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		assertEquals(x.getDepth(),40);
+	}
+	@Test
+	public void getWidthTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		assertEquals(x.getWidth(),20);
+	}
+	@Test
+	public void getLengthTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		assertEquals(x.getLength(),30);
+	}
+	@Test
+	public void setDepthTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		x.setDepth(50);
+		assertEquals(x.getDepth(),50);
+	}
+	@Test
+	public void setWidthTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		x.setWidth(50);
+		assertEquals(x.getWidth(),50);
+	}
+	@Test
+	public void setLengthTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		x.setLength(50);
+		assertEquals(x.getLength(),50);
+	}
+	@Test
+	public void areaTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		assertEquals((int)x.area(),5200);
+	}
+	@Test
+	public void volumeTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		assertEquals((int)x.volume(),24000);
+	}
+	@Test (expected=UnsupportedOperationException.class)
+	public void perimeterTest() {
+		Cuboid x=new Cuboid(20,30,40);
+		x.perimeter();
+	}
+	@Test (expected=IllegalArgumentException.class)
+	public void constructorTest1() {
+		Cuboid x=new Cuboid(20,30,-40);
+	}
+	@Test (expected=IllegalArgumentException.class)
+	public void constructorTest2() {
+		Cuboid x=new Cuboid(20,30,0);
+	}
+	@Test (expected=IllegalArgumentException.class)
+	public void constructorTest3() {
+		Cuboid x=new Cuboid(-20,30,40);
+	}
+	@Test (expected=IllegalArgumentException.class)
+	public void constructorTest4() {
+		Cuboid x=new Cuboid(0,30,40);
+	}
+	@Test (expected=IllegalArgumentException.class)
+	public void constructorTest5() {
+		Cuboid x=new Cuboid(20,-30,40);
+	}
+	@Test (expected=IllegalArgumentException.class)
+	public void constructorTest6() {
+		Cuboid x=new Cuboid(20,0,40);
+	}
+}
